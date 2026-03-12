1e843fb500cd56114003d0a2af1a30286974acf9
Ayush Prajapati <ayushprajpati6@gmail.com>
2026-03-12 08:54:47 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function loadFooter(){
+
+fetch("footer.html")
+
+.then(response => response.text())
+
+.then(data => {
+
+document.getElementById("footer").innerHTML = data;
+
+});
+
+}
+
+loadFooter();
